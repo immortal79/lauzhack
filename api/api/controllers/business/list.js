c440220a4a656157b8c59c7b403ca61b8cf295b5
valentinT1 <tamtam.valentin@gmail.com>
2020-11-28 15:36:40 +0100

    search business by name

--- api/api/controllers/business/list.js
+++ api/api/controllers/business/list.js
@@ -1,27 +1,38 @@
 module.exports = {
 
 
-  friendlyName: 'List',
+  friendlyName: 'List Business',
 
-
-  description: 'List business.',
+  description: 'Lists business containing user input.',
 
 
   inputs: {
-
+    search: {
+      description: 'Search input from user',
+      type: 'string',
+      required: false
+    },
   },
-
 
   exits: {
-
+    badRequest: {
+      description: 'Identifier input error.',
+      responseType: 'badRequest'
+    },
+    success: {
+      description: 'Successful search',
+      responseType: 'ok'
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
+    let result = await Business.find({
+      where: {name: {contains: inputs.search}},
+      limit: 15
+    });
 
-    // All done.
-    return;
-
+    return exits.success(result);
   }
 
 
