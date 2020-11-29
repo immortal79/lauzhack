f885eeab912727c1bef4bb353f23c48ec7c59059
MagicMustache <milo.gianinazzi@gmail.com>
2020-11-29 11:38:47 +0100

    login screen

--- front-business/src/components/Home.js
+++ front-business/src/components/Home.js
@@ -1,39 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
-import {
+import  {
     BrowserRouter as Router,
     Switch,
+    Redirect,
     Route,
     Link
 } from "react-router-dom";
-import { Col, Row } from 'react-bootstrap';
+import {Alert, Col, Form, Row} from 'react-bootstrap';
+import API from "../API";
 
 function Home() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [failed,setFailed] = useState(false)
+    const [ok,setOk] = useState(false)
+
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        API.post("business/login",{email:email,password:password}).then((res)=>{
+            setOk(true)
+        }).catch((e)=>{
+            setFailed(true)
+        })
+    }
+
     return (
         <Row className="justify-content-md-center text-center">
-            <Col sm lg={2} className="py-2">
-                <Link to={"/signup"}>
-                    <Button>
-                        Signup
-                    </Button>
-                </Link>
-            </Col>
-
-            <Col sm lg={2} className="py-2">
-            <Link to={"/dispo"}>
-                <Button>
-                    Disponibilities
+            <Form onSubmit={handleSubmit} style={{marginTop:"20%"}}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        name={"email"}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        name={"pwd"}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Link to={"/signup"} >Signup</Link>
+                {failed ? <Alert variant={"danger"}>Login infos not correct</Alert> : null}
+                {ok ? <Redirect to={"/dispo"}/> : null}
+                <Button block size="lg" type="submit" disabled={!validateForm()} style={{marginTop:"1em"}}>
+                    Login
                 </Button>
-            </Link>
-            </Col>
-
-            <Col sm lg={2} className="py-2">
-            <Link to={"/bookings"}>
-                <Button>
-                    Bookings
-                </Button>
-            </Link>
-            </Col>
+            </Form>
         </Row>
     );
 }
