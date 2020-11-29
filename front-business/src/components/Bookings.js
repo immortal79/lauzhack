8f4ad1b2021eda19a17cc1d7939905e1e5657524
MagicMustache <milo.gianinazzi@gmail.com>
2020-11-29 10:44:26 +0100

    added unique slots taken with number of occurences

--- front-business/src/components/Bookings.js
+++ front-business/src/components/Bookings.js
@@ -18,10 +18,20 @@ function Bookings(){
         let query = "slot/list?filter=taken&date="+string
         console.log(query)
         API.get(query,{}).then((res)=>{
-            setDaysBooked(res.data);
             console.log(res.data)
+            setDaysBooked(res.data.data);
         })
     }
+    function duplicates(startDate){
+        let dupli = 0;
+        for(let i=0;i<daysBooked.length;i++){
+            if(daysBooked[i].start === startDate){
+                dupli+=1
+            }
+        }
+        return dupli
+    }
+    let toNotDisplay = []
     return(
         <Container style={{}}>
             <h1 style={{textAlign:"center",marginBottom:"2em", paddingTop:"2em"}}>ShopSafe</h1>
@@ -34,8 +44,17 @@ function Bookings(){
                 />
                 <br/>
                 {daysBooked.length>0 ? <span>Slots taken for this day :</span> : <span>No slots taken for this day</span>}
+                <br/>
                 {daysBooked.map((data,id)=>{
-                    return <span>{data.reservationStart} - {data.reservationEnd}</span>
+                    if(!toNotDisplay.includes(data.start)){
+                        if(duplicates(data.start)<2){
+                            return <span>{data.start} - {data.end}<br/></span>
+                        }
+                        else{
+                            toNotDisplay.push(data.start)
+                            return <span>{data.start} - {data.end} -> {duplicates(data.start)} times<br/></span>
+                        }
+                    }
                 })}
             </div>
             <Link to={"/dispo"} style={{position:"relative",left:"50em",top:"2em"}}>
